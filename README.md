a963a04abe3c54a3f7d2ce6958fb1e97a811f06b
Justin Golden <justingolden21@gmail.com>
2021-07-30 15:40:07 -0700

    init : )

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# three-n-plus-one
-an interactive an analysis of the collatz conjecture
+# three n plus one
+
+### About
+
+an analysis of the collatz conjecture
+
+### Links
+
+- Live demo: https://justingolden.me/three-n-plus-one
+
+- This repo: https://github.com/justingolden21/three-n-plus-one
+
+<hr>
+
+- My website: https://justingolden.me
+
+- My repos: https://github.com/justingolden21
+
+- Contact me: contact@justingolden.me
---
+++ js/scripts.js
@@ -0,0 +1,64 @@
+// const max = 300;
+
+window.onload = () => {
+	u('#submit').on('click', () => {
+		const start = parseInt(u('#start').val()) || 1;
+		const end = parseInt(u('#end').val()) || 1;
+
+		const showSeq = u('#show-seq').first().checked;
+		const showSeqLen = u('#show-seq-len').first().checked;
+		const showSeqBar = u('#show-seq-bar').first().checked;
+
+		let html = '';
+		let lens = [];
+		for (let i = start; i <= end; i++) {
+			const arr = threeNPlusOne(i);
+			html += `<b>${i}</b>: 
+			${showSeqLen ? 'Length: ' + arr.length + '<br>' : ''} 
+			${showSeq ? 'Sequence: ' + arr.join(', ') + '<br>' : ''}
+			${
+				showSeqBar
+					? `<div class="bar" style="width:${
+							arr.length * 8
+					  }px"></div>`
+					: ''
+			} `;
+
+			// if (arr.length > max) console.log(i, arr.length);
+			lens.push(arr.length);
+		}
+
+		html =
+			`Min Length: ${Math.min(...lens)}.
+		Max Length: ${Math.max(...lens)}.
+		Average Length: ${average(lens)}<br><br>` + html;
+
+		u('#out').html(html);
+	});
+
+	u('input').on('keydown', (e) => {
+		if (e.key == 'Enter') u('#submit').trigger('click');
+	});
+
+	u('#submit').trigger('click');
+
+	u('#end').first().select();
+};
+
+function threeNPlusOne(x) {
+	let n = x;
+	let seq = [];
+	while (n != 1) {
+		if (n % 2 == 0) {
+			n /= 2;
+		} else {
+			n = 3 * n + 1;
+		}
+		seq.push(n);
+
+		// if (seq.length > max) return x + ' is over ' + max + ' in length';
+	}
+	return seq;
+}
+
+const average = (arr) => arr.reduce((a, b) => a + b, 0) / arr.length;
